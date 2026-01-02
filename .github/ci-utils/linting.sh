029bfe68d2a8daf118156edd22f64e7da5953345
Lucas Burigo <burigolucas@gmail.com>
2026-01-02 23:40:51 +0100

    fix: fix issues with secrets

--- .github/ci-utils/linting.sh
+++ .github/ci-utils/linting.sh
@@ -2,6 +2,7 @@
 
 export SARIF_REPORT_FULL_PATH="./code-quality-report.sarif"
 export LINT_ARGS="--profile production"
+
 if [[ "["tests/integration/", "tests/unit"]" != "" ]]; then
     FLAG=" --exclude"
     EXCLUDE_ARGS=$(echo '["tests/integration/", "tests/unit"]' | tr -d '[],"')
@@ -10,16 +11,21 @@ if [[ "["tests/integration/", "tests/unit"]" != "" ]]; then
         LINT_ARGS+=" $FLAG $item"
     done
 fi
+
 if true ; then LINT_ARGS+=" --parseable"; fi
 if true ; then LINT_ARGS+=" --force-color"; fi
 if false ; then LINT_ARGS+=" --offline"; fi
 if true ; then LINT_ARGS+=" --strict"; fi
 if true ; then LINT_ARGS+=" --sarif-file ${SARIF_REPORT_FULL_PATH}"; fi
+
 ansible-lint --version
+
 echo "Running ansible-lint with the following parameters"
 echo "${LINT_ARGS}"
+
 echo "Switch directories to project directory ."
 cd .
+
 ansible-lint ${LINT_ARGS} || RETURN=$?
 if true && ! [ -f ${SARIF_REPORT_FULL_PATH} ] ; then
     echo "Code quality sarif report was requested but not generated, please check the logs."
